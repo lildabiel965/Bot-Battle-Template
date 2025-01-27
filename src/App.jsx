45b547c2015de78eab869f337f4505050fcffdd1
jeycee <jeyceejeyka635@gmail.com>
2025-01-27 15:19:40 +0300

    removed gitIgnores

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import BotCollection from './BotCollection';
 import YourBotArmy from './YourBotArmy';
-// import SortBar from './SortBar';
 import './App.css'
 
 function App() {
